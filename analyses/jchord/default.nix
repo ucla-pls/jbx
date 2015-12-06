02e2c012238bd6faf7a407cacc223a2a8d7d6ac3
Christian Gram Kalhauge <christian@kalhauge.dk>
2015-12-06 00:42:59 -0800

    Added bddbddb to jchord

--- analyses/jchord/default.nix
+++ analyses/jchord/default.nix
@@ -4,23 +4,33 @@ rec {
     env:
     options @ {
       subanalysis
+      , datalog ? true
     }:
     benchmark: 
-    mkLogicBloxAnalysis rec {
-      name = "jchord-${subanalysis}-${benchmark.name}";
-      analysis = ./jchord.sh;
-      inherit env;
+    let 
+      options = rec {
+        name = "jchord-${if datalog then "dlog" else "bddbddb"}-${subanalysis}-${benchmark.name}";
+        analysis = ./jchord.sh;
+        inherit env;
 
-      jchord = tools.jchord;
-      jre = benchmark.java.jre;
-      inherit (benchmark) mainclass build libraries;
+        jchord = tools.jchord;
+        jre = benchmark.java.jre;
+        inherit (benchmark) mainclass build libraries;
 
-      settings = ''chord.main.class=${benchmark.mainclass}
-        chord.run.analyses=${subanalysis}
-        chord.datalog.engine=logicblox4
-        chord.err.file=/dev/stderr
-        chord.out.file=/dev/stdout'';
-    };
+        settings = ''
+chord.main.class=${benchmark.mainclass}
+chord.run.analyses=${subanalysis}
+${if datalog then "chord.datalog.engine=logicblox4" else ""}
+chord.err.file=/dev/stderr
+chord.out.file=/dev/stdout
+'';
+      };
+    in 
+    if datalog then 
+      mkLogicBloxAnalysis options
+    else
+      mkAnalysis options
+
 
   cipa-0cfa-dlog = env: base env { subanalysis = "cipa-0cfa-dlog"; };
   deadlock = env: base env { subanalysis = "deadlock-java"; };
