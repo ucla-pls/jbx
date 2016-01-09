cea41ecb17448410b96d20a70457c35cb7f9d95a
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-01-08 16:28:48 -0800

    Make batik support java 5 too

--- benchmarks/dacapo/batik/default.nix
+++ benchmarks/dacapo/batik/default.nix
@@ -7,8 +7,8 @@ let
       md5 = "dac6a6ce70013c839434e31563fea660";
     };
     buildInputs = [ ant java.jdk subversion ];
+    ANT_OPTS="-Xms256m -Xmx256m";
     buildPhase = "
-      ls $src
       ant all-jar
     ";
     installPhase = ''
@@ -23,6 +23,7 @@ in {
   build = batik;
   data = "${daCapoSrc}/benchmarks/bms/batik/data/batik/";
   # libraries = java: with java.libs; [ xalan xerces];
+  filter = java: builtins.elem java.version [5 6];
   inputs = [
     { name = "small";
       args = [
@@ -33,40 +34,3 @@ in {
     }
   ];
 }
-
-# let
-#   # This package needs jdk6
-#   pureBuild = stdenv.mkDerivation rec {
-#     name = "batik";
-#     version = "1.7beta1";
-#     src = fetchzip {
-#       url = ''http://archive.apache.org/dist/xmlgraphics/batik/batik-src-${version}.zip'';
-#       md5 = "dac6a6ce70013c839434e31563fea660";
-#       #packed = "fb1f6a7e996d44e417398e2f5067658b";
-#     };
-#     buildInputs = [ ant jdk7 ];
-#     buildPhase = "ant all-jar";
-#     installPhase = ''
-#       find . -name *.jar;
-#     '';
-#   };
-#   buildAlternative = stdenv.mkDerivation rec { 
-#     name = "batik";
-#     version = "1.7";
-#     src = fetchzip { 
-#       url = "http://apache.osuosl.org/xmlgraphics/batik/source/batik-src-${version}.zip";
-#       md5 = "ca88e823c57d10611aa0839570711661";
-#     };
-#     buildInputs = [ ant jdk7 ];
-#     buildPhase = "ant all-jar";
-#     installPhase = ''
-#       mkdir -p $out/share/java
-#       mv batik-${version}/lib/batik-all.jar $out/share/java
-#     '';
-#   };
-# in {
-#     name = "batik_${toString jversion_}";
-#     build = pureBuild;
-#     jversion = jversion_;
-#     inputs = [];
-# }
