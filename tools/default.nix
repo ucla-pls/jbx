8d3b4ee49c0cfb5612ff90568a0ca06e624a6040
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-02-25 15:38:41 -0800

    Upgraded logicblox

--- tools/default.nix
+++ tools/default.nix
@@ -8,14 +8,21 @@ rec {
   jre5 = jdk5;
   jre6 = jdk6; # Not cool but works.
 
-  inherit (pkgs.callPackage ./logicblox {fetchprop = fetchprop;}) logicblox-3_10_21 logicblox-4_2_0;
-  logicblox = logicblox-4_2_0;
+  inherit (pkgs.callPackage ./logicblox {fetchprop = fetchprop;}) 
+    logicblox-3_10_21 
+    logicblox-4_2_0
+    logicblox-4_3_6_3
+  ;
+  logicblox = logicblox-4_3_6_3;
+
   doop = pkgs.callPackage ./doop { inherit logicblox-3_10_21; };
+  
   inherit (pkgs.callPackage ./jchord {}) 
     jchord-head
     jchord-2_0
   ;
   jchord = jchord-head;
+  
   inherit (pkgs.callPackage ./petablox {}) 
     petablox-0_1
     petablox-1_0
--- tools/logicblox/default.nix
+++ tools/logicblox/default.nix
@@ -1,36 +1,46 @@
 { stdenv, fetchprop, jdk7, makeWrapper }:
-{
-logicblox-3_10_21 = stdenv.mkDerivation {
+let lb4 =
+options @ {
+  version,
+  url ? "logicblox-${version}.tar.gz",
+  md5 ? "00000000000000000000000000000000",
+}: stdenv.mkDerivation {
   name = "logicblox";
-  version = "3.10.21";
-  src = fetchprop {
-    url = "logicblox-3.10.21.tar.gz";
-    md5 = "75611acbc5f6fdd48f22e2a68809b1d4";
-  };
-  phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
-  installPhase = ''
-    cp -r logicblox/* $out
-  '';
-};
-logicblox-4_2_0 = stdenv.mkDerivation {
-  name = "logicblox";
-  version = "4.2.0";
-  src = fetchprop {
-    url = "logicblox-4.2.0.tar.gz";
-    md5 = "b179aa5df2d74830bbbf845a82f831da";
-  };
+  inherit version;
+  src = fetchprop { inherit url md5; };
   phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
   buildInputs = [ makeWrapper ];
   installPhase = ''
-    mkdir $out
-    cp -r * $out
+  mkdir $out
+  cp -r * $out
   '';
 
   postFixup = ''
-    find $out -type f -perm -0100 \
-        -exec patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" {} \;
-    wrapProgram $out/bin/lb \
-      --prefix PATH : ${jdk7}/bin/
+  find $out -type f -perm -0100 \
+  -exec patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" {} \;
+  wrapProgram $out/bin/lb \
+  --prefix PATH : ${jdk7}/bin/
   '';
 };
+in {
+  logicblox-3_10_21 = stdenv.mkDerivation {
+    name = "logicblox";
+    version = "3.10.21";
+    src = fetchprop {
+      url = "logicblox-3.10.21.tar.gz";
+      md5 = "75611acbc5f6fdd48f22e2a68809b1d4";
+    };
+    phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+    installPhase = ''
+    cp -r logicblox/* $out
+    '';
+  };
+  logicblox-4_2_0 = lb4 {
+    version = "4.2.0";
+    md5 = "b179aa5df2d74830bbbf845a82f831da";
+  };
+  logicblox-4_3_6_3 = lb4 {
+    version = "4.3.6.3";
+    md5 = "2c2e66ab6209698a6d8e23bca38daa32";
+  };
 }
