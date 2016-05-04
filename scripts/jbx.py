cebd6eccc04884c230dfb6a4b358154bbd19b021
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-05-03 17:13:45 -0700

    Work done on jbx.py

--- scripts/jbx.py
+++ scripts/jbx.py
@@ -13,6 +13,8 @@ CURRENTFOLDER = path.dirname(path.abspath(CURRENTFILE))
 JBXFOLDER = path.dirname(CURRENTFOLDER)
 JBXPATH = path.join(JBXFOLDER, "expr", "default.nix")
 
+from functools import partial
+
 import nixutils
 from funcparse import *
 
@@ -22,35 +24,13 @@ def quote(item):
 def to_nix_list(list): 
    return "[{}]".format(" ".join(map(quote, list))) 
 
-def to_benchmark_list(list): 
-    return "lib.attrsets.attrVals {list} jbx.benchmarks.byName".format(
-        list=to_nix_list(list)
-    )
-
-def by_tag(tag):
-    return "jbx.benchmarks.byTag.{}".format(tag)
-
-benchmarkSelector = OneOf(
-    only = ListOf("-o", 
-        help = "a list of benchmarks", 
-        action=to_benchmark_list
-    ),
-    tag = Arg("-t", 
-        help = "a single tag", 
-        action=by_tag
-    )
-)
-
-def format_transformer(transformer):
-    if transformer is None:
-        return "b: b"
-    else:
-        return "b: jbx.transformers.{} b".format(transformer)
-
-transformerSelector = Arg("-T",
-    help = "a transforms benchmark, from jbx.transformers",
-    action=format_transformer
-) 
+def from_attrset_list(attrset):
+    def from_attrset(list):
+        return "lib.attrsets.attrVals {list} {attrset}".format(
+            attrset = attrset,
+            list = to_nix_list(list)
+        )
+    return from_attrset
 
 LIST_CMD = """
 let
@@ -79,155 +59,224 @@ def list(
         print(item)
 
 
-BUILD_CMD = """
-let
-  jbx = import {filename} {{}};
-  inherit (jbx.utils) withJava;
-  java = jbx.java.java{java};
-  lib = jbx.pkgs.lib;
-in 
-  map (b: {fetch}) (
-    map (withJava java) (
-      map ({transformer}) (
-        {benchmarks}
-      )
-    )
-  )
-"""
-def build(
-        benchmarks : benchmarkSelector,
-        transformer: transformerSelector = None,
-        src_only : Arg(None, 
-            help = "only fetch the source of the benchmark."
-        ) = False,
-        **opts
-    ):
-    """Builds a benchmark"""
-    
-    fetch = "b.build.src" if src_only else "b.build"
-
-    cmd = BUILD_CMD.format(
-        fetch = fetch, 
-        benchmarks = benchmarks, 
-        transformer = transformer,
-        **opts
-    )
-    nixutils.build(cmd, **opts)
-
 RUN_CMD = """
 let
   jbx = import {filename} {{}};
-  env = import {environment};
-  inherit (jbx.utils) withJava analyseInput;
   java = jbx.java.java{java};
+  
+  env = import {environment};
   lib = jbx.pkgs.lib;
-in 
-  map (b: analyseInput jbx.analyses.{analysis} b env "{input}") (
-    map (withJava java) (
-      map ({transformer}) (
-        {benchmarks}
-      )
-    )
-  )
+
+  benchmark = {benchmark};
+  transformers = {transformers};
+
+  transformed = 
+    builtins.foldl' (b': t: t b') benchmark transformers;
+  
+  input = {input};
+
+in jbx.analyses.{analysis} (transformed.withJava java) env input 
 """
 def run(
-    input : Arg("-i", help = "the input to the dynamic analysis"),
-    benchmarks : benchmarkSelector,
-    transformer: transformerSelector = None,
-    analysis : Arg("-a", help = "the dynamic analyisis to run") = "run.run",
+    
+    benchmark: 
+        Arg(None, 
+            help = "name of the benchmark to run",
+            action="jbx.benchmarks.byName.{}".format 
+        ),
+
+    transformers:
+        ListOf("-T", 
+            help = "transformers in order",
+            action=from_attrset_list("jbx.transformers")
+        )
+    = "[]",
+
+    input_name: 
+        Arg("-i", help = "the input to the dynamic analysis")
+    = "",
+
+    analysis: 
+        Arg("-a", help = "the dynamic analyisis to run") 
+    = "run.run",
+
+    java: 
+        Arg("-j", help = "Java version") 
+    = 6,
+    
+    debug: 
+        Arg("-d", help = "add debug settings to the run") 
+    = False,
+    
+    environment: 
+        Arg("-E", help = "the user environment to run things in") 
+    = path.join(JBXFOLDER, "environment.nix"),
+    
+    keep_failed: 
+        Arg("-K", help = "keeps the output even if the output fails.") 
+    = False,
+    
+    dry_run: 
+        Arg("-n", help = "do not execute, print nix command instead.") 
+    = False,
+
+    *args, 
+
     **opts
     ):
-    """ run an dynamic analysis """
+    """ run a dynamic analysis """
+
+    if input_name:
+        input = "getInput transformed {!r}".format(input_name)
+    else:
+        input = "{{ name = \"cmdline\"; args = {}; }}".format(to_nix_list(args))
 
     cmd = RUN_CMD.format(
-        benchmarks = benchmarks,
+        benchmark = benchmark,
         input = input,
-        transformer = transformer,
+        environment = environment,
+        transformers = transformers,
+        java = java,
         analysis = analysis,
         **opts
     )
-    nixutils.build(cmd, **opts)
+    
+    nixutils.build(
+        cmd,
+        debug = debug,
+        dry_run = dry_run, 
+        keep_failed = keep_failed,
+        **opts
+    )
 
 ANALYSE_CMD = """
 let
   jbx = import {filename} {{}};
-  env = import {environment};
-  inherit (jbx.utils) withJava analyseInput;
   java = jbx.java.java{java};
+  
+  env = import {environment};
   lib = jbx.pkgs.lib;
-in 
-  map (b: jbx.analyses.{analysis} b env ) (
-    map (withJava java) (
-      map ({transformer}) (
-        {benchmarks}
-      )
-    )
-  )
+
+  benchmarks = {benchmarks};
+  transformers = {transformers};
+
+  analysis = {analysis};
+
+  transformed = 
+    map 
+      (b: builtins.foldl' (b': t: t b') b transformers) 
+      benchmarks;
+
+  results = 
+    map 
+      (b: analysis (b.withJava java) env) 
+      transformed;
+
+in results
 """
 def analyse(
-    benchmarks : benchmarkSelector,
-    transformer: transformerSelector = None,
-    analysis : Arg("-a", help = "the analysis to run") = "run.runAll",
-    **opts
-    ):
-    """ run an dynamic analysis """
 
+    benchmarks: 
+        OneOf(
+            only = ListOf("-o", 
+                help = "a list of benchmarks", 
+                action = from_attrset_list("jbx.benchmarks.byName")
+            ),
+            tag = Arg("-t", 
+                help = "a single tag", 
+                action="jbx.benchmarks.byTag.{}".format
+            )
+        ),
+
+    transformers:
+        ListOf("-T", 
+            help = "transformers in order",
+            action=from_attrset_list("jbx.transformers")
+        )
+    = "[]",
+
+    analysis:
+        Arg("-a", 
+            help = "analysis of choice",
+            action="jbx.analyses.{}".format 
+        )
+    = "(b: env: b)", # do nothing
+        
+    java: 
+        Arg("-j", help = "Java version") 
+    = 6,
+    
+    debug: 
+        Arg("-d", help = "add debug settings to the run") 
+    = False,
+    
+    environment: 
+        Arg("-E", help = "the user environment to run things in") 
+    = path.join(JBXFOLDER, "environment.nix"),
+    
+    keep_failed: 
+        Arg("-K", help = "keeps the output even if the output fails.") 
+    = False,
+    
+    dry_run: 
+        Arg("-n", help = "do not execute, print nix command instead.") 
+    = False,
+
+    **opts
+    ): 
+    """ runs a series of analyses one or more benchmarks. """
+    
     cmd = ANALYSE_CMD.format(
+        java = java,
+        environment = environment,
         benchmarks = benchmarks,
-        transformer = transformer,
+        transformers = transformers,
         analysis = analysis,
+        keep_failed = keep_failed,
+        dry_run = dry_run,
         **opts
     )
-    nixutils.build(cmd, **opts)
+    nixutils.build(
+        cmd,
+        debug = debug,
+        dry_run = dry_run, 
+        keep_failed = keep_failed,
+        **opts
+    )
+
 
 TOOL_CMD = """
 let jbx = import {filename} {{}};
 in jbx.pkgs.{tool}
 """
 def tool(
-    tool : Arg(None, help="the tool to install"), 
-    **opts
+        tool : Arg(None, help="the tool to install"), 
+        **opts
     ):
+    """ test a tool."""
     cmd = TOOL_CMD.format(tool=tool, **opts)
     nixutils.build(cmd, **opts)
 
 def main(
-        command : SubCommands(
-            build, list, run, tool, analyse,
+    command : 
+        SubCommands(
+            analyse, run, list, tool,
             help = "available sub-commands"        
-            ),
-        java : Arg("-j",
-            help = "Java version"
-        ) = 6,
-        filename : Arg("-f", 
+        ),
+        
+    filename: 
+        Arg("-f", 
             help = "the path to the jbx default.nix file."
-        ) = JBXPATH,
-        keep_failed : Arg("-K", 
-            help = "keeps the output even if the output fails."
-        ) = False,
-        dry_run : Arg("-n",
-            help = "do not execute, print nix command instead."
-        ) = False,
-        debug : Arg("-d",
-            help = "add debug settings to the run"
-        ) = False,
-        environment : Arg("-E",
-            help = "the user environment to run things in",
-        ) = path.join(JBXFOLDER, "environment.nix")
+        ) 
+    = JBXPATH,
+    
     ): 
     """Jbx is a collection of tools that helps you writing nix scripts
     for working with jbx.
     """
-    print(command)
     return command(
-        java = java, 
-        keep_failed = keep_failed, 
-        dry_run = dry_run,
         filename = filename,
-        environment = environment,
-        debug = debug
     )
-
 
 if __name__ == "__main__":
     parse_args(main)()
