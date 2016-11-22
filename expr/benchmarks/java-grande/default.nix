c1335f080662ee9eac0f42863d886ab19e849856
Duyet Nguyen <duyetlnguyen@gmail.com>
2016-11-21 16:05:23 -0800

    Refactor and add another benchmark

--- expr/benchmarks/java-grande/default.nix
+++ expr/benchmarks/java-grande/default.nix
@@ -1,6 +1,6 @@
 { callPackage, utils }:
 let
-  inherit (utils) callBenchmark fetchprop;
+  inherit (utils) callBenchmark mkBenchmarkTemplate fetchprop;
   # Same as:
   # callBenchmark = utils.callBenchmark;
   # fetchprop = utils.fetchprop;
@@ -9,14 +9,45 @@ let
      sha256="10mfasrbga7zvf51pybvp1cwwllm26i6c2v6w0gyay69nsb5sh91";
   };
 in rec {
-  src = java-grande-src;
+  build = java: {
+    src = java-grande-src;
+    phases = ["unpackPhase" "buildPhase" "installPhase"];
+    buildInputs = [ java.jdk ];
+    unpackPhase = ''
+      tar -xzvf $src
+    '';
+    buildPhase = ''
+      cd java_grande
+      mkdir java-grande-section3
+      javac src/jgfutil/*.java \
+        section3/montecarlo/*.java \
+        section3/moldyn/*.java \
+        section3/raytracer/*.java \
+        section3/JGFMonteCarloBenchSizeA.java \
+        section3/JGFMonteCarloBenchSizeB.java \
+        section3/JGFMolDynBenchSizeA.java \
+        section3/JGFMolDynBenchSizeB.java \
+        section3/JGFRayTracerBenchSizeA.java \
+        section3/JGFRayTracerBenchSizeB.java \
+        -d java-grande-section3
+      jar cvf java-grande-section3.jar -C java-grande-section3 .
+    '';
+    installPhase = ''
+      mkdir -p $out/share/java
+      mv java-grande-section3.jar $_
+      mkdir -p $out/data
+      cp section3/Data/hitData $_/
+    '';
+  };
 
-  monte-carlo = callBenchmark ./monte-carlo { inherit java-grande-src; };
+  monte-carlo-A = mkBenchmarkTemplate ((callPackage ./monte-carlo { inherit build; }) "A");
+  monte-carlo-B = mkBenchmarkTemplate ((callPackage ./monte-carlo { inherit build; }) "B");
   ray-tracer = callBenchmark ./ray-tracer { inherit java-grande-src; };
   mol-dyn = callBenchmark ./mol-dyn { inherit java-grande-src; };
 
   all = [
-    monte-carlo
+    monte-carlo-A
+    monte-carlo-B
     # ray-tracer
     # mol-dyn
   ];
--- expr/benchmarks/java-grande/monte-carlo/default.nix
+++ expr/benchmarks/java-grande/monte-carlo/default.nix
@@ -1,39 +1,18 @@
-{ java-grande-src }:
-rec {
-  name = "monte-carlo";
-  mainclass = "JGFMonteCarloBenchSizeA";
-  build = java: {
-    src = java-grande-src;
-    phases = ["unpackPhase" "buildPhase" "installPhase"];
-    buildInputs = [ java.jdk ];
-    unpackPhase = ''
-      tar -xzvf $src
-    '';
-    buildPhase = ''
-      cd java_grande
-      mkdir monte-carlo
-      javac src/jgfutil/*.java \
-        section3/montecarlo/*.java \
-        section3/JGFMonteCarloBenchSizeA.java \
-        section3/JGFMonteCarloBenchSizeB.java \
-        -d monte-carlo
-      jar cvf monte-carlo.jar -C monte-carlo .
-    '';
-    installPhase = ''
-      mkdir -p $out/share/java
-      mv monte-carlo.jar $_
-      mkdir -p $out/data
-      cp section3/Data/hitData $_/
-    '';
+{ build }:
+let
+  bm = name: {
+    name = "monte-carlo-${name}";
+    mainclass = "JGFMonteCarloBenchSize${name}";
+    build = build;
+    inputs = [
+      {
+        name ="default";
+        args = [
+          "5"
+          "$data/data"
+          "hitData"
+        ];
+      }
+    ];
   };
-  inputs = [
-    {
-      name ="default";
-      args = [
-        "5"
-        "$data/data"
-        "hitData"
-      ];
-    }
-  ];
-}
+in bm
