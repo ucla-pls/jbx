165f8fb39087969bb6401647d62764e50b96a80b
Duyet Nguyen <duyetlnguyen@gmail.com>
2016-11-22 11:17:53 -0800

    More refactoring + add mol-dyn

--- expr/benchmarks/java-grande/default.nix
+++ expr/benchmarks/java-grande/default.nix
@@ -8,46 +8,93 @@ let
      url="java_grande.tar.gz";
      sha256="10mfasrbga7zvf51pybvp1cwwllm26i6c2v6w0gyay69nsb5sh91";
   };
+  java-grande-benchmark = {
+    name,
+    mainclass,
+    inputs
+  }: mkBenchmarkTemplate {
+    inherit name mainclass inputs;
+    build = java: {
+      src = java-grande-src;
+      phases = ["unpackPhase" "buildPhase" "installPhase"];
+      buildInputs = [ java.jdk ];
+      unpackPhase = ''
+        tar -xzvf $src
+      '';
+      buildPhase = ''
+        cd java_grande
+        mkdir java-grande-section3
+        javac src/jgfutil/*.java \
+          section3/montecarlo/*.java \
+          section3/moldyn/*.java \
+          section3/raytracer/*.java \
+          section3/JGFMonteCarloBenchSizeA.java \
+          section3/JGFMonteCarloBenchSizeB.java \
+          section3/JGFMolDynBenchSizeA.java \
+          section3/JGFMolDynBenchSizeB.java \
+          section3/JGFRayTracerBenchSizeA.java \
+          section3/JGFRayTracerBenchSizeB.java \
+          -d java-grande-section3
+        jar cvf java-grande-section3.jar -C java-grande-section3 .
+      '';
+      installPhase = ''
+        mkdir -p $out/share/java
+        mv java-grande-section3.jar $_
+        mkdir -p $out/data
+        cp section3/Data/hitData $_/
+      '';
+    };
+  };
 in rec {
-  build = java: {
-    src = java-grande-src;
-    phases = ["unpackPhase" "buildPhase" "installPhase"];
-    buildInputs = [ java.jdk ];
-    unpackPhase = ''
-      tar -xzvf $src
-    '';
-    buildPhase = ''
-      cd java_grande
-      mkdir java-grande-section3
-      javac src/jgfutil/*.java \
-        section3/montecarlo/*.java \
-        section3/moldyn/*.java \
-        section3/raytracer/*.java \
-        section3/JGFMonteCarloBenchSizeA.java \
-        section3/JGFMonteCarloBenchSizeB.java \
-        section3/JGFMolDynBenchSizeA.java \
-        section3/JGFMolDynBenchSizeB.java \
-        section3/JGFRayTracerBenchSizeA.java \
-        section3/JGFRayTracerBenchSizeB.java \
-        -d java-grande-section3
-      jar cvf java-grande-section3.jar -C java-grande-section3 .
-    '';
-    installPhase = ''
-      mkdir -p $out/share/java
-      mv java-grande-section3.jar $_
-      mkdir -p $out/data
-      cp section3/Data/hitData $_/
-    '';
+  monte-carlo-a = java-grande-benchmark {
+    name = "monte-carlo-a";
+    mainclass = "JGFMonteCarloBenchSizeA";
+    inputs = [ {
+      name ="default";
+      args = [
+        "5"
+        "$data/data"
+        "hitData"
+      ];
+    } ];
   };
 
-  monte-carlo-A = mkBenchmarkTemplate ((callPackage ./monte-carlo { inherit build; }) "A");
-  monte-carlo-B = mkBenchmarkTemplate ((callPackage ./monte-carlo { inherit build; }) "B");
-  ray-tracer = callBenchmark ./ray-tracer { inherit java-grande-src; };
-  mol-dyn = callBenchmark ./mol-dyn { inherit java-grande-src; };
+  monte-carlo-b = java-grande-benchmark {
+    name = "monte-carlo-b";
+    mainclass = "JGFMonteCarloBenchSizeB";
+    inputs = [ {
+      name ="default";
+      args = [
+        "5"
+        "$data/data"
+        "hitData"
+      ];
+    } ];
+  };
+
+  mol-dyn-a = java-grande-benchmark {
+    name = "mol-dyn-a";
+    mainclass = "JGFMolDynBenchSizeA";
+    inputs = [ {
+      name ="default";
+      args = [ "5" ];
+    } ];
+  };
+
+  mol-dyn-b = java-grande-benchmark {
+    name = "mol-dyn-b";
+    mainclass = "JGFMolDynBenchSizeB";
+    inputs = [ {
+      name ="default";
+      args = [ "5"];
+    } ];
+  };
 
   all = [
-    monte-carlo-A
-    monte-carlo-B
+    monte-carlo-a
+    monte-carlo-b
+    mol-dyn-a
+    mol-dyn-b
     # ray-tracer
     # mol-dyn
   ];
