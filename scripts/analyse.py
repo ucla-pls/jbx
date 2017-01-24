fe8c2ffe133d8ae486646eccab1c2072d0808e20
Colin Terndrup <cterndrup@cs.ucla.edu>
2017-01-24 10:06:43 -0800

    add json flag to analyse

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -8,6 +8,48 @@ This module allows us to run analyses on a list of benchmarks.
 from functools import partial
 from funcparse import *
 import nixutils
+import benchmark
+import fetch
+
+def test(info, transformers, analysis, opts):
+    expr = benchmark.nixexpr(info)
+
+    test_expr = ANALYSE_JSON_CMD.format(
+        expr = "\n".join("    " + e for e in expr.split("\n")),
+        transformers = transformers,
+        analysis = analysis,
+        **opts
+    );
+
+    nixutils.build(test_expr, **opts)
+
+ANALYSE_JSON_CMD = """
+let
+  jbx = import {filename} {{}};
+  java = jbx.java.java{java};
+
+  env = import {environment};
+  lib = jbx.pkgs.lib;
+
+  benchmarkTemplate = {expr};
+
+  benchmarks = (jbx.pkgs.callPackage benchmarkTemplate {{}}).all;
+  transformers = {transformers};
+
+  analysis = {analysis};
+
+  transformed =
+    map
+      (b: builtins.foldl' (b': t: t b') b transformers)
+      benchmarks;
+
+  results =
+    map
+      (b: analysis (b.withJava java) env)
+      transformed;
+
+in results
+"""
 
 ANALYSE_CMD = """
 let
@@ -34,6 +76,7 @@ let
 
 in results
 """
+
 def analyse(
 
     benchmarks:
@@ -45,6 +88,10 @@ def analyse(
             tag = Arg("-t",
                 help = "a single tag",
                 action="jbx.benchmarks.byTag.{}".format
+            ),
+            json = Arg(None,
+                help = "a json object representing the benchmark",
+                action=fetch.parse_json
             )
         ),
 
@@ -66,14 +113,28 @@ def analyse(
     ):
     """ runs a series of analyses one or more benchmarks. """
 
-    cmd = ANALYSE_CMD.format(
-        benchmarks = benchmarks,
-        transformers = transformers,
-        analysis = analysis,
-        **opts
-    )
-    nixutils.build(
-        cmd,
-        keep_going = not opts["short_curcuit"],
-        **opts
-    )
+    # JSON code path
+    if (type(benchmarks) is dict):
+        repo = fetch.fetchobj(benchmarks, **opts)
+    
+        info = benchmark.populate({
+            "repo": repo,
+            "subfolder": "",
+            "sha256": ""
+        }, **opts)
+    
+        test(info, transformers, analysis, opts)
+
+    # NON-JSON code path
+    else:
+        cmd = ANALYSE_CMD.format(
+            benchmarks = benchmarks,
+            transformers = transformers,
+            analysis = analysis,
+            **opts
+        )
+        nixutils.build(
+            cmd,
+            keep_going = not opts["short_curcuit"],
+            **opts
+        )
