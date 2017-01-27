2407b44d3953a66de03ad69957bcc259911ce39b
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-01-26 18:31:42 -0800

    Added more deadlocks to sir benchmark.

--- expr/benchmarks/sir/default.nix
+++ expr/benchmarks/sir/default.nix
@@ -1,17 +1,18 @@
-{ utils }:
+{ utils, unzip, cpio }:
 let
   base = utils.fetchprop {
     url = "sir.tar.gz";
-    sha256 = "11yl8zv7wpkgl7khfj50djyx8829flwdk35g87y2l7xa6xy78zh5";
+    sha256 = "11lgsy6lbw1vh4kxbrkj22q1fx3h6x59i32cx43max9664k7rbsr";
   };
 
   sirBenchmark =
     { name
-    , mainclass
+    , mainclass ? "Main"
     , inputs ? [
       { name = "default"; args = []; }
     ]
     , folder ? null
+    , srcfolder ? "versions.alt/"
     , tags ? []
     }:
     utils.mkBenchmarkTemplate {
@@ -20,14 +21,30 @@ let
       build = java: {
         src = base;
         phases = "unpackPhase buildPhase installPhase";
-        buildInputs = [ java.jdk ];
+        buildInputs = [ java.jdk unzip cpio];
+        srcfolder = srcfolder;
         unpackPhase = ''
           tar -xzf $src
           cd "sir/${if builtins.isNull folder then name else folder}"
         '';
         buildPhase = ''
-          mkdir -p "$out/info" "$out/src" "$out/classes"
-          for file in $(find versions.alt/ -name '*.java'); do
+          mkdir -p "$out/info" "$out/src" "$out/classes" "$out/lib"
+
+          if [ -e ./lib ]; then
+            for path in $(find ./lib -name "*.jar"); do
+                echo "Copying over classpath $path..."
+                if [[ $path == *.jar ]]; then
+                    mkdir _out
+                    unzip -qq -o "$path" -d _out
+                    path=_out
+                fi
+                (cd "$path";
+                find . -name "*.class" | sort | cpio --quiet -updm $out/lib)
+                if [[ -e _out ]]; then rm -r _out; fi
+            done
+          fi
+
+          for file in $(find $srcfolder -name '*.java'); do
               path=$(sed -n '
                 /^[[:space:]]*package [[:space:][:alnum:].].*;/{
                   s/.*package//g
@@ -43,6 +60,8 @@ let
           cd $out
 
           find src -name '*.java' | sort > info/sources
+
+          cat info/sources
 
           javac -encoding UTF-8 -cp classes:lib -d classes @info/sources
 
@@ -66,11 +85,13 @@ in rec {
   account = sirBenchmark {
     name = "account";
     mainclass = "Main";
+    tags = ["deadlock"];
   };
 
   accountsubtype = sirBenchmark {
     name = "accountsubtype";
     mainclass = "Main";
+    tags = ["deadlock"];
   };
 
   airline = sirBenchmark {
@@ -94,6 +115,7 @@ in rec {
         args = ["1" "4" "4" "2"];
       }
     ];
+    tags = ["deadlock"];
   };
 
   clean = sirBenchmark {
@@ -104,11 +126,115 @@ in rec {
         args = ["1" "1" "12"];
       }
     ];
+    tags = ["deadlock"];
   };
 
   deadlock = sirBenchmark {
     name = "deadlock";
     mainclass = "Deadlock";
+    tags = ["deadlock"];
+  };
+
+  diningPhilosophers = sirBenchmark {
+    name = "diningPhilosophers";
+    mainclass = "DiningPhilosophers";
+    tags = ["deadlock"];
+  };
+
+  groovy = sirBenchmark {
+    name = "groovy";
+    srcfolder = "versions.alt/fixed";
+    tags = ["deadlock"];
+  };
+
+  log4j1-fixed = sirBenchmark {
+    name = "log4j1-fixed";
+    folder = "log4j1";
+    srcfolder = "versions.alt/fixed";
+    mainclass = "org.apache.log4j.test.UnitTestAppender";
+    tags = ["deadlock"];
+  };
+
+  log4j1-orig = sirBenchmark {
+    name = "log4j1-orig";
+    folder = "log4j1";
+    srcfolder = "versions.alt/orig";
+    mainclass = "org.apache.log4j.test.UnitTestAppender";
+    tags = ["deadlock"];
+  };
+
+  loseNotify = sirBenchmark {
+    name = "loseNotify";
+    tags = ["deadlock"];
+    inputs = [
+      { name = "default";
+        args = ["1" "1" "12"];
+      }
+    ];
+  };
+
+  nestedmonitor = sirBenchmark {
+    name = "nestedmonitor";
+    tags = ["deadlock"];
+    srcfolder = "versions.alt/orig";
+    mainclass = "NestedMonitor";
+  };
+
+  piper = sirBenchmark {
+    name = "piper";
+    tags = ["deadlock"];
+    mainclass = "IBM_Airlines";
+    inputs = [
+      { name = "default";
+        args = ["1" "32" "2"];
+      }
+    ];
+  };
+
+  pool4 = sirBenchmark {
+    name = "pool4";
+    mainclass = "org.apache.commons.pool.impl.TestGenericObjectPool";
+    tags = ["deadlock"];
+  };
+
+  pool5 = sirBenchmark {
+    name = "pool5";
+    mainclass = "org.apache.commons.pool.impl.TestGenericObjectPool";
+    tags = ["deadlock"];
+  };
+
+  pool6 = sirBenchmark {
+    name = "pool6";
+    mainclass = "org.apache.commons.pool.impl.TestGenericObjectPool";
+    tags = ["deadlock"];
+  };
+
+  readerswriters = sirBenchmark {
+    name = "readerswriters";
+    mainclass = "RWVSNDriver";
+    tags = ["deadlock"];
+    inputs = [
+      { name = "default";
+        args = ["2" "2" "100"];
+      }
+    ];
+  };
+
+  replicatedworkers = sirBenchmark {
+    name = "replicatedworkers";
+    tags = ["deadlock"];
+    mainclass = "apps.AdaptiveQuadrature.Main";
+    inputs = [
+      { name = "default";
+        args = ["5" "2" "0" "10" "0.05"];
+      }
+    ];
+  };
+
+  sleepingBarber = sirBenchmark {
+    name = "sleepingBarber";
+    tags = ["deadlock"];
+    mainclass = "SleepingBarber";
   };
 
   all = [
@@ -119,5 +245,18 @@ in rec {
     boundedBuffer
     clean
     deadlock
+    diningPhilosophers
+    groovy
+    log4j1-orig
+    log4j1-fixed
+    loseNotify
+    nestedmonitor
+    piper
+    pool4
+    pool5
+    pool6
+    readerswriters
+    replicatedworkers
+    sleepingBarber
   ];
 }
