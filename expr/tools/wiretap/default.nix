a6d491eb3fc5295b6f079c8cc2f0a36079c7cb55
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-01-31 18:00:54 -0800

    Upgrade wiretap

--- expr/tools/wiretap/default.nix
+++ expr/tools/wiretap/default.nix
@@ -4,8 +4,8 @@
     name = "wiretap";
     src = fetchgit {
       url = "https://github.com/ucla-pls/wiretap.git";
-      rev = "6351ba20b07f92a531f48ec1051195b20a4cbb4b";
-      sha256 = "0w9asp2ph6lzk8rlc159xs21x706y426l7krgpdd22p0h25jg4kd";
+      rev = "d61ebafcaec0227616261e797fda490ad37bf35e";
+      sha256 = "0f850a1sk345av31d8xjnbl336zk8h19wci957dza6kinr80js3m";
       branchName = "develop";
     };
     buildInputs = [ unzip ant java.jdk ];
