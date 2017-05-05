e34400810e35a77835959c316b5215072b18a6b4
Colin Terndrup <cterndrup@cs.ucla.edu>
2017-05-05 16:02:50 -0700

    add petabloxDynamic and reflection callers comparison

--- expr/analyses/reflection/default.nix
+++ expr/analyses/reflection/default.nix
@@ -1,5 +1,6 @@
 { utils, reachable-methods, python3 }:
 let
+  extract-methods = ./reflection-callers.sed;
   caller-methods = ./reflection-callers.sh;
   compare = ./reflection-comp.py;
 in rec {
@@ -7,14 +8,21 @@ in rec {
       name = "reflection-caller-methods";
       tools = [ benchmark.java.jdk ];
       analysis = ''
-        analyse "reflection-caller-methods" bash ${caller-methods}
+        analyse "reflection-caller-methods" \
+            sed -nEf ${extract-methods} < <(javap -c -p -s -classpath $classpath $(jar -tf $classpath | grep "class$" | sed 's/\.class//g')) | \
+            sort -u | tee "$out/upper"
       '';
   } benchmark;
+
+  reflection-overview = utils.overview "reflection-overview" [
+    reachable-methods.wiretapAll
+    reachable-methods.petabloxDynamic
+  ];
 
   comp = benchmark: env: utils.mkAnalysis {
     name = "reflection-comp";
     reflection_callers = (reflection-caller-methods benchmark env).out;
-    reachable_methods = (reachable-methods.overview benchmark env).out;
+    reachable_methods = (reflection-overview benchmark env).out;
     tools = [ python3 ];
     analysis = ''
       analyse "reflection-comp" python3 ${compare}
