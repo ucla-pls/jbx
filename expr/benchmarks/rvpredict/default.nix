9bfcc014fcfba059781d345aaa1fe1e44c358b9f
Christian Gram Kalhauge <christian@kalhauge.dk>
2018-02-28 11:04:20 -0800

    Added more benchmarks

---
+++ expr/benchmarks/rvpredict/default.nix
@@ -0,0 +1,75 @@
+{ utils, cpio }:
+let 
+  base = utils.fetchprop {
+    url = "rvpredict-benchmarks.tar.gz";
+    sha256 = "09xfm3j8jj6q55h0arlfd7girbykbk1ff576zzkfvjm3739qbd7i";
+  };
+
+  rvp = 
+    { name
+    , mainclass ? "Main"
+    , inputs ? [ { name = "default"; args = [];} ]
+    , tags ? []
+    }: 
+    utils.mkBenchmarkTemplate {
+      name = "rvpredict-${name}";
+      tags = tags ++ ["rvpredict"];
+      inherit mainclass inputs;
+      build = java: {
+        src = base;
+        phases = "unpackPhase patchPhase buildPhase installPhase";
+        buildInputs = [ java.jdk cpio ];
+        buildPhase = ''
+          mkdir -p "$out/info" "$out/classes" "$out/lib"
+          cp -r "$src" "$out/src"
+          
+          find src -name '*.java' | sort > info/sources
+          javac -encoding UTF-8 -cp classes:lib -d classes @info/sources
+
+          classes=$(find classes -name "*.class" | sed 's/.class//;s/\//./g;s/classes.//' | sort );
+          echo "$classes" | sed "s/ /\n/g" > info/classes
+          javap -classpath classes $classes > info/declarations
+
+          # Finding mainclasses
+          sed -e '/.*public static .*void main(java.lang.String\[\])/{g;p;}' \
+              -e 's/.*class \([[:alnum:].]*\).*/\1/;T;h' \
+              -n info/declarations > info/mainclasses
+        '';
+        installPhase = ''
+          mkdir -p $out/share/java
+          jar cf $_/rvpredict-benchmark.jar -C classes .
+          jar uf $out/share/java/rvpredict-benchmark.jar -C lib .
+        '';
+      };
+    };
+in rec {
+  bufwriter = rvp { name = "bufwriter"; mainclass = "bufwriter.BufWriter";};
+  account = rvp { name = "account"; mainclass = "account.Account";};
+  airlinetickets = rvp { name = "airlinetickets"; mainclass = "airlinetickets.Airlinetickets ";};
+  array = rvp { name = "array"; mainclass = "array.Test ";};
+  boundedbuffer = rvp { name = "boundedbuffer"; mainclass = "boundedbuffer.BoundedBuffer";};
+  mergesort = rvp { name = "mergesort"; mainclass = "mergesort.MergeSort";};
+  pingpong = rvp { name = "pingpong"; mainclass = "pingpong.PingPong";};
+  critical = rvp { name = "critical"; mainclass = "critical.Critical";};
+
+  JGFMolDynBenchSizeA = rvp { name = "JGFMolDynBenchSizeA"; mainclass = "benchmarks.JGFMolDynBenchSizeA";};
+  JGFMonteCarloBenchSizeA = rvp { name = "JGFMonteCarloBenchSizeA"; mainclass = "benchmarks.JGFMonteCarloBenchSizeA";};
+  JGFRayTracerBenchSizeA = rvp { name = "JGFRayTracerBenchSizeA"; mainclass = "benchmarks.JGFRayTracerBenchSizeA";};
+
+  all = [
+    bufwriter 
+    account 
+    airlinetickets 
+    array 
+    boundedbuffer 
+    mergesort 
+    pingpong 
+    critical 
+
+    JGFMolDynBenchSizeA 
+    JGFMonteCarloBenchSizeA 
+    JGFRayTracerBenchSizeA 
+  ];
+}
+
+
