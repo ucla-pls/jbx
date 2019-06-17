cb60c24149f4ea7665ad1ebe5c83b52337bd599f
Christian Kalhauge <kalhauge@cs.ucla.edu>
2019-06-17 14:26:10 -0700

    Update WALA

--- expr/analyses/call-graph-edges/default.nix
+++ expr/analyses/call-graph-edges/default.nix
@@ -15,26 +15,37 @@ rec {
         name = "wala-call-graph-edges";
         buildInputs = [ java makeWrapper ];
         phases = "installPhase";
+        walajars = "${wala}/share/java/core.jar:${wala}/share/java/util.jar:${wala}/share/java/shrike.jar";
         installPhase = ''
          mkdir $out
          cp ${./WalaCallgraph.java} WalaCallgraph.java
-         javac -cp ${wala}/share/java/core.jar:${wala}/share/java/util.jar:${wala}/share/java/shrike.jar WalaCallgraph.java -d $out
+         javac -cp $walajars WalaCallgraph.java -d $out
          mkdir $out/bin
          makeWrapper ${java}/bin/java $out/bin/wala-call-graph-edges \
           --add-flags -cp \
-          --add-flags ${wala}/share/java/core.jar:${wala}/share/java/util.jar:${wala}/share/java/shrike.jar:$out\
+          --add-flags $walajars:$out\
           --add-flags walaOptions.WalaCallgraph\
         '';
       };
   wala-call-graph-edges8 = wala-call-graph-edges openjdk8;
 
-  wala = { name, args }: b: 
+  wala = { 
+    reflection ? true,
+    resolveinterfaces ? true,
+    analysis ? "0cfa",
+    }: b: 
     utils.mkAnalysis {
-      name = "wala-call-graph-edges-${name}";
+      name = "wala-cge-${analysis}-${(if reflection then "" else "no")
++ "reflect"}-${if resolveinterfaces then "intf" else "nointf"}";
       tools = [ (wala-call-graph-edges b.java.jdk) ];
       timelimit = 1800;
       analysis = ''
-        analyse "wala-${name}" wala-call-graph-edges -classpath $classpath -mainclass $mainclass -output edges.txt ${args}
+        analyse "$name" wala-call-graph-edges \
+          -classpath $classpath -mainclass $mainclass \
+          -output edges.txt \
+          -analysis ${analysis} \
+          -reflection ${if reflection then "true" else "false"} \
+          -resolveinterfaces ${if resolveinterfaces then "true" else "false"}
       '';
       postprocess = ''
         if [ -f $sandbox/edges.txt ]
@@ -44,13 +55,48 @@ rec {
       '';
   } b ;
 
-  wala-0cfa-noreflect = wala { name = "0cfa-noreflect"; args = "-analysis 0cfa -reflection false -resolveinterfaces true";};
-  wala-0cfa-reflect = wala { name = "0cfa-noreflect"; args = "-analysis 0cfa -reflection true -resolveinterfaces true";};
-  wala-1cfa-noreflect = wala { name = "0cfa-noreflect"; args = "-analysis 1cfa -reflection false -resolveinterfaces true";};
-  wala-1cfa-reflect = wala { name = "0cfa-noreflect"; args = "-analysis 1cfa -reflection true -resolveinterfaces true";};
-  wala-rta-noreflect = wala { name = "0cfa-noreflect"; args = "-analysis rta -reflection false -resolveinterfaces true";};
-  wala-0cfa-noreflect-no_interface_resolution = wala { name = "0cfa-noreflect"; args = "-analysis 0cfa -reflection false -resolveinterfaces false";};
+  wala-0cfa-noreflect = 
+    wala { 
+      reflection = false;
+      resolveinterfaces = true;
+      analysis = "0cfa";
+    };
+  
+  wala-0cfa-reflect = 
+    wala { 
+      reflection = true;
+      resolveinterfaces = true;
+      analysis = "0cfa";
+    };
 
+  wala-1cfa-noreflect = 
+    wala { 
+      reflection = false;
+      resolveinterfaces = true;
+      analysis = "1cfa";
+    };
+  
+  wala-1cfa-reflect = 
+    wala { 
+      reflection = true;
+      resolveinterfaces = true;
+      analysis = "1cfa";
+    };
+  
+  wala-rta-noreflect = 
+    wala { 
+      reflection = false;
+      resolveinterfaces = true;
+      analysis = "rta";
+    };
+
+  wala-0cfa-noreflect-nointf = 
+    wala { 
+      reflection = false;
+      resolveinterfaces = false;
+      analysis = "0cfa";
+    };
+  
   doop = reflection_: shared.doop { 
     subanalysis = "context-insensitive";
     doop = tools.doop;
